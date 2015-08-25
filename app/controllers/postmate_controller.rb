53c139fdf4b4512d656cc6cdee261e38d86b0409
Joshua Echeverri <josheche@gmail.com>
2015-08-25 11:32:53 -0400

    added postmates ruby wrapper to postmate controller

--- app/controllers/postmate_controller.rb
+++ app/controllers/postmate_controller.rb
@@ -1,16 +1,26 @@
-require 'httparty'
-require 'json'
+require 'postmates'
 
 class PostmateController < ApplicationController
   def get_delivery
-    api_key = INSERT API KEY HERE
-    @customer = CUSTOMER ID STRING HERE
-    @urlstring_to_post = 'https://api.postmates.com/v1/customers/' + @customer + ROUTE FOR ORDER HERE
+  # Create a new Postmates client
+  @client = Postmates.new
 
-    @result = HTTParty.post(@urlstring_to_post.to_str,
-      :body => { :dropoff_address => "400 NW 26th Street, Miami, FL, 33127",
-                 :pickup_address => ADDRESS FOR PANTHER COFFEE HERE
-                }
-      :basic_auth => { :username => api_key })
+  # Set basic config variables
+  @client.configure do |config|
+    config.api_key = 'YOUR_API_KEY'
+    config.customer_id = 'YOUR_CUSTOMER_ID'
   end
+
+  # Or do some more advanced stuff
+  @client.configure do |config|
+    # Get full Faraday responses instead of Ruby objects representing the body
+    config.raw_response = true
+    # Ensure consistent fields & formats by specifying a version in the header
+    config.headers.merge!('X-Postmates-Version' => 20140825)
+    # Work with a possible future version of the API
+    config.api_url = 'https://api.postmates.com/v2/'
+  end
+
+  # And switch back to the defaults easily
+  @client.reset
 end
